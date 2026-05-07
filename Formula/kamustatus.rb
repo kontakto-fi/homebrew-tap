12498e1ea9d3c68072621f3f652b5a33c0372f59
Tommi Savikko <tommi.savikko@kontakto.fi>
2026-05-07 23:03:44 +0300

    Use Tigris CDN for public binary downloads

--- Formula/kamustatus.rb
+++ Formula/kamustatus.rb
@@ -1,11 +1,13 @@
 class Kamustatus < Formula
   desc "Multi-region uptime monitoring CLI"
-  homepage "https://github.com/kontakto-fi/kamustatus"
+  homepage "https://kamustatus.com"
   version "0.1.0"
+
+  CDN = "https://kamustatus-releases.fly.storage.tigris.dev/cli/v#{version}"
 
   on_macos do
     on_arm do
-      url "https://github.com/kontakto-fi/kamustatus/releases/download/v#{version}/kamustatus-darwin-arm64"
+      url "#{CDN}/kamustatus-darwin-arm64"
       sha256 "dc47875675c04abac15730185e6ec30dcc15868d1bba5e3219974e3a847303a7"
 
       def install
@@ -14,7 +16,7 @@ class Kamustatus < Formula
     end
 
     on_intel do
-      url "https://github.com/kontakto-fi/kamustatus/releases/download/v#{version}/kamustatus-darwin-amd64"
+      url "#{CDN}/kamustatus-darwin-amd64"
       sha256 "22b708df04c865065d2bd085f50ef8ae5ed19192534c894c688c5e8da9487332"
 
       def install
@@ -25,7 +27,7 @@ class Kamustatus < Formula
 
   on_linux do
     on_arm do
-      url "https://github.com/kontakto-fi/kamustatus/releases/download/v#{version}/kamustatus-linux-arm64"
+      url "#{CDN}/kamustatus-linux-arm64"
       sha256 "dafdbd70e0ff7e458269d9dbd86efa71a8bfe3880d32d05e04d1deb93aed782d"
 
       def install
@@ -34,7 +36,7 @@ class Kamustatus < Formula
     end
 
     on_intel do
-      url "https://github.com/kontakto-fi/kamustatus/releases/download/v#{version}/kamustatus-linux-amd64"
+      url "#{CDN}/kamustatus-linux-amd64"
       sha256 "b08ed219312a46dbb2214e760f88288144d1aff9dd3573b2b72534523aa6f9b8"
 
       def install
